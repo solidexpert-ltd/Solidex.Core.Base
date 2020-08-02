6b48ae6ef86626271420639f0b27f3e5a50bc8dc
siarhei shchypanau <breate.by@gmail.com>
2020-08-03 02:09:57 +0300

    Ientity

---
+++ Abstraction/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Solidex.Core.Base.Abstraction
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime ModificationDate { get; set; }
+    }
+}
