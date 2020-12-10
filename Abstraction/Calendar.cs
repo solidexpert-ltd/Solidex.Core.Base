e123a8f4c3eec02ad97c3c3fefa14fc0aad32876
Юрик Хмелюк <yurick.hmeluk@gmail.com>
2020-12-10 10:57:30 +0300

    remove unusing

--- Abstraction/Calendar.cs
+++ Abstraction/Calendar.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace Solidex.Core.Base.Abstraction
 {
--- Translate/TranslateToLatin.cs
+++ Translate/TranslateToLatin.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Xml;
 
 namespace Solidex.Core.Base.Translate
 {
